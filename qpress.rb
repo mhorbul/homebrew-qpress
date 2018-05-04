70c302e53aaaf4d0c542da1ed90fb900edb7531d
Maksim Horbul <max@gorbul.net>
2018-05-04 11:17:40 -0700

    add qpress formula

---
+++ qpress.rb
@@ -0,0 +1,12 @@
+class QPress < Formula
+  desc "Portable high-speed file archiver"
+  homepage "https://github.com/PierreLvx/qpress"
+  url "https://github.com/PierreLvx/qpress/releases/download/20170415/qpress.tar.gz"
+  sha256 "de16a5faee7ff219b0fb42fdbf36d549530299f99f1d73d943bec22bf9aeb6ad"
+  version "20170415"
+
+  def install
+    system "make", "g++"
+    bin.install "qpress"
+  end
+end
